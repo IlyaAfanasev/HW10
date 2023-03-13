e0d0d52bc2079f382a505b45e9c53672421409ce
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-13 20:16:06 +0400

    task 2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,7 @@ public static void task1 (){
     String lastName = "Ivanov";
     String fullName = lastName+" "+firstName+" "+middleName;
     System.out.println("ФИО сотрудника — " + fullName);
+    System.out.println("     Задача 2");
+    System.out.println("Данные ФИО сотрудника для заполнения отчета —"+fullName.toUpperCase());
     }
 }
